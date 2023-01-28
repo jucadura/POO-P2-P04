8d3eba5f4961fbad5d193479f11ce635b4326d62
jucadura <93133974+jucadura@users.noreply.github.com>
2023-01-28 00:15:36 -0500

    Add files via upload

---
+++ Proyecto2P/Proyecto_2P_G2/src/main/java/ClasesNormales/Cliente.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ClasesNormales;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Cliente {
+    private String nombre;
+    private String contraseña;
+    public Cliente(String nombre, String contraseña){
+        this.nombre = nombre;
+        this.contraseña = contraseña;
+    
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the contraseña
+     */
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    /**
+     * @param contraseña the contraseña to set
+     */
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+}
