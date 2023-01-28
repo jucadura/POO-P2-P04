db6c457bd6d0e78c90e7d274ef82bab351d63064
jucadura <93133974+jucadura@users.noreply.github.com>
2023-01-28 00:14:07 -0500

    Delete Proyecto_2P_G2 directory

--- Proyecto_2P_G2/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.mycompany.proyecto_p2_g2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.proyectop2_g2 to javafx.fxml;
-    exports com.mycompany.proyectop2_g2;
-}
