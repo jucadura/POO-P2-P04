1153689f01cd7344006394687ee98c6e6c414240
jucadura <93133974+jucadura@users.noreply.github.com>
2023-01-28 00:27:31 -0500

    Delete Proyecto2P/Proyecto_2P_G2 directory

--- Proyecto2P/Proyecto_2P_G2/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.pooespol.mavenproject1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.pooespol.mavenproject1 to javafx.fxml;
-    exports com.pooespol.mavenproject1;
-}
