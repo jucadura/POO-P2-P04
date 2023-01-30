494794666e4bc6c6a5b2af3f45d6585f56aecc9a
CarlosCrespoC <caedcres@espol.edu.ec>
2023-01-29 22:29:58 -0500

    Archivo Local Java, finalización de package restaurant

---
+++ Proyecto2P/Proyecto2P/src/main/java/logica/Local.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logica;
+
+/**
+ *
+ * @author Michael
+ */
+public class Local {
+    private String nombre;
+    private double coordenadaX;
+    private double coordenadaY;
+    private String dirección;
+    private String horario;
+
+    public Local(String nombre, double coordenadaX, double coordenadaY, String dirección, String horario) {
+        this.nombre = nombre;
+        this.coordenadaX = coordenadaX;
+        this.coordenadaY = coordenadaY;
+        this.dirección = dirección;
+        this.horario = horario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getCoordenadaX() {
+        return coordenadaX;
+    }
+
+    public void setCoordenadaX(double coordenadaX) {
+        this.coordenadaX = coordenadaX;
+    }
+
+    public double getCoordenadaY() {
+        return coordenadaY;
+    }
+
+    public void setCoordenadaY(double coordenadaY) {
+        this.coordenadaY = coordenadaY;
+    }
+
+    public String getDirección() {
+        return dirección;
+    }
+
+    public void setDirección(String dirección) {
+        this.dirección = dirección;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+}
