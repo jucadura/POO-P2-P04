636ae1fe8e21407eb3573f036e0a510f16e9fe1d
CarlosCrespoC <caedcres@espol.edu.ec>
2023-01-29 22:30:55 -0500

    Clase Menu finalizada v Final Crespo

---
+++ Proyecto2P/Proyecto2P/src/main/java/logica/Menu.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logica;
+
+/**
+ *
+ * @author Michael
+ */
+public class Menu implements Comparable<Menu> {
+
+    private String descripcion;
+    private double precio;
+    private Tipo tipo;
+
+    public Menu(String descripcion, double precio, Tipo tipo) {
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.tipo = tipo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public int compareTo(Menu otroMenu) {
+        return Double.compare(this.precio, otroMenu.precio);
+    }
+    
+}
